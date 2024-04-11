07bae8778b0d92d510cc85057e4c151b22aa493b
GyuBeom <yonglong15@gmail.com>
2024-04-11 17:42:16 +0900

    Member function

--- ObstancleAssault/Source/ObstancleAssault/MovingPlatform.cpp
+++ ObstancleAssault/Source/ObstancleAssault/MovingPlatform.cpp
@@ -28,27 +28,31 @@ void AMovingPlatform::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
-	// 플랫폼 앞으로 이동
-		// 현재 위치 얻기
+	MovePlatform(DeltaTime);
+	RotatePlatform(DeltaTime);
+}
+
+void AMovingPlatform::MovePlatform(float DeltaTime)
+{
 	FVector CurrentLocation = GetActorLocation();
-		// 벡터를 그 위치에 더함
 	CurrentLocation = CurrentLocation + PlatformVelocity * DeltaTime;
-		// 그 위치를 설정
 	SetActorLocation(CurrentLocation);
-	// 플랫폼이 너무 멀리가면 돌아오도록 설정
-		// 얼마나 이동했는지 확인
 	float DistanceMoved = FVector::Dist(StartLocation, CurrentLocation);
-		// 너무 멀리 갔다면 움직임 방향 반대로 변경
+
 	if (DistanceMoved > MoveDistance)
 	{
 		float OverShoot = DistanceMoved - MoveDistance;
-		UE_LOG(LogTemp, Display, TEXT("%s overshot by %f"), *Name, OverShoot);
+		UE_LOG(LogTemp, Display, TEXT("%s overshot distance: %f"), *Name, OverShoot);
 
 		FVector MoveDirection = PlatformVelocity.GetSafeNormal();
 		StartLocation = StartLocation + MoveDirection * MoveDistance;
 		SetActorLocation(StartLocation);
 		PlatformVelocity *= -1;
 	}
+}
+
+void AMovingPlatform::RotatePlatform(float DeltaTime)
+{
 
 }
 
--- ObstancleAssault/Source/ObstancleAssault/MovingPlatform.h
+++ ObstancleAssault/Source/ObstancleAssault/MovingPlatform.h
@@ -24,6 +24,10 @@ public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
+private:
+	void MovePlatform(float DeltaTime);
+	void RotatePlatform(float DeltaTime);
+
 	UPROPERTY(EditAnywhere, Category = "Moving Platform")
 	FVector PlatformVelocity = FVector(100, 0, 0);
 	UPROPERTY(EditAnywhere, Category = "Moving Platform")
