5a8e57fdb3a4d0bed07ae7ac47ddcd6d6143ee47
GyuBeom Han <sy0871@naver.com>
2024-06-10 16:01:25 +0900

    Get overlapping actors

--- CryptRaider/Source/CryptRaider/TriggerComponent.cpp
+++ CryptRaider/Source/CryptRaider/TriggerComponent.cpp
@@ -13,11 +13,17 @@ UTriggerComponent::UTriggerComponent()
 void UTriggerComponent::BeginPlay()
 {
 	Super::BeginPlay();
-
-	UE_LOG(LogTemp, Display, TEXT("Hello, World"));
 }
 
 void UTriggerComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	TArray<AActor*> Actors;
+	GetOverlappingActors(Actors);
+	
+	for (auto Actor : Actors)
+	{
+		UE_LOG(LogTemp, Display, TEXT("Overlapping: %s"), *Actor->GetActorNameOrLabel());
+	}
 }
